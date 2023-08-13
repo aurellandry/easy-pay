5720d5c8e5ba18a17316ed967f8600c5c8d7db9a
Aurel Landry KENGNI NKONGTCHOU <landrykengni@yahoo.com>
2023-08-13 12:54:41 +0100

    feat(backend): setup mongodb and routes

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+import connectDB from '../config/db.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import userRoutes from './routes/userRoutes.js';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+connectDB();
+
+app.use('/api/users', userRoutes);
+app.get('/', (req, res) => {
+  res.send('Server is ready');
+});
+
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(port, () => {
+  console.log(`Easy-pay app listening on port ${port}!`);
+});
