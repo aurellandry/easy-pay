3f71096a75d244f1c502409833735162548d55f2
Aurel Landry KENGNI NKONGTCHOU <landrykengni@yahoo.com>
2023-08-14 16:29:49 +0100

    feat(frontend): setup redux store

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,12 +6,14 @@ import {
   Route,
   RouterProvider
 } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
-import App from './App.jsx';
-import HomeScreen from './screens/HomeScreen.jsx';
-import LoginScreen from './screens/LoginScreen.jsx';
+import App from './App';
+import HomeScreen from './screens/HomeScreen';
+import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
 
 const router = createBrowserRouter(
@@ -25,7 +27,9 @@ const router = createBrowserRouter(
 );
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={ router } />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <RouterProvider router={ router } />
+    </React.StrictMode>
+  </Provider>
 )
